6458559b2e6bc42a19b2651599e567ee765c308c
Piotr Pelc <piotrpelc83@gmail.com>
2025-05-19 11:29:02 +0200

    Add middlewares

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(cors());
+
+app.use('api',adsRoutes)
+app.use('api',usersRoutes)
+app.use('api',adsRoutes)
+
+upp.use(express.static(path.join(__dirname, 'client/build')));
 
 app.listen(8000, () => {
   console.log('Server is running..');
