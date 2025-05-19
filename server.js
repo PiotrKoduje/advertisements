19f95a34ef98fab47cd2590b7d8d42b9cc902e55
Piotr Pelc <piotrpelc83@gmail.com>
2025-05-19 11:45:55 +0200

    Add Mongoose to project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,11 +10,16 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(cors());
 
-app.use('api',adsRoutes)
-app.use('api',usersRoutes)
-app.use('api',adsRoutes)
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-upp.use(express.static(path.join(__dirname, 'client/build')));
+// CONNECT TO MONGOOSE
+const uri = 'mongodb://0.0.0.0:27017/adsDB';
+mongoose.connect(uri, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Connected to local database');
+});
+db.on('error', err => console.log('Error ' + err));
 
 app.listen(8000, () => {
   console.log('Server is running..');
